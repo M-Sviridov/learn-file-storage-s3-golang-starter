efc0b9bdfd9b171e677bfeda8c8ceb52eedfecf5
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-11-26 12:24:17 +0700

    Complete up to CH3 L6 Architecture of the boot.dev project

--- assets.go
+++ assets.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/google/uuid"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
@@ -16,9 +16,15 @@ func (cfg apiConfig) ensureAssetsDir() error {
 	return nil
 }
 
-func getAssetPath(videoID uuid.UUID, mediaType string) string {
+func getAssetPath(mediaType string) string {
+	data := make([]byte, 32)
+	_, err := rand.Read(data)
+	if err != nil {
+		os.Exit(1)
+	}
+	prefix := base64.RawURLEncoding.EncodeToString(data)
 	ext := mediaTypeToExt(mediaType)
-	return fmt.Sprintf("%s%s", videoID, ext)
+	return fmt.Sprintf("%s%s", prefix, ext)
 }
 
 func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
